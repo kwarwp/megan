31c92967edfdb87464960cdc10555f371b26e165
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:59:58 -0300

    Saved lisa/main.py at Timestamp: 2018-10-19 14:59:58

--- lisa/main.py
+++ lisa/main.py
@@ -2,7 +2,7 @@
 from _spy.vitollino.main import Cena, Elemento, Texto
 Barbie =
 Odiariodabarbie = Cena(imghttp://cf2.imgobject.com/t/p/original/iMT2XJgzSKjDfVrIi3pO35UcbMc.jpg
-    Barbie= Elemento(img, 
+    Barbie= Elemento(https://vignette.wikia.nocookie.net/barbie-movies/images/b/b4/25fda09be7e14d882431cd0560b3c94e.jpg/revision/latest?cb=20140723073415
            tit = Tifany
            style = dict(left=150, top=60,width=60, height=200))
     Barbie=(cenaTeatro)
