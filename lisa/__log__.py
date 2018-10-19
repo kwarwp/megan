b42dbd3278e260c1c7801f577b54e82f3ae1342f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:31:52 -0300

    Created lisa/__log__.py at Timestamp: 2018-10-19 14:31:52

---
+++ lisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Oct 19 2018 11:31:51.754 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  Goku = Elemento(img,
+  ^
+IndentationError: unexpected indent
+'''},
