a93921274246d801221af2b97c51924c245220fd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:43:40 -0300

    Saved lisa/main.py at Timestamp: 2018-10-19 14:43:40

--- lisa/main.py
+++ lisa/main.py
@@ -1,12 +1,11 @@
 # Carol
 from _spy.vitollino.main import Cena, Elemento, Texto
-
-GOKU ="https://i.ytimg.com/vi/jEjtNGZnsak/hqdefault.jpg"
-cenaTeatro = Cena(img ="http://1.bp.blogspot.com/-aGwacYjRKw8/T-St9kEVvMI/AAAAAAAAAvs/ximTg6O8XUA/s1600/ESCENARIOS29+%282%.jpg")
-    Goku = Elemento(img,
+Barbie ="https://i.ytimg.com/vi/jEjtNGZnsak/hqdefault.jpg"
+O diário da barbie = Cena(img ="http://1.bp.blogspot.com/-aGwacYjRKw8/T-St9kEVvMI/AAAAAAAAAvs/ximTg6O8XUA/s1600/ESCENARIOS29+%282%.jpg")
+    Barbie= Elemento(img, 
            tit ="Goku",
            style = dict(left=150, top=60,width=60, height=200))
-    goku.entra(cenaTeatro)
+    Barbie=(cenaTeatro)
     cena
     História 
      
