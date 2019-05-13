26f5d10c1229d606693b76f8a4c9992c8c779ce4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-13 11:16:49 -0300

    Saved meredith/__log__.py at Timestamp: 2019-05-13 14:16:48

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -6,3 +6,10 @@
                                    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 20 2009 02:21:19.724 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  Thor = Elemento (img = Thor,tit "Thor",Style=dict(left=150,top=160,width=60,height=200))
+                                   ^
+SyntaxError: invalid syntax
+'''},
