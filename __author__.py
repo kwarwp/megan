a84ad119b210ecb81b4c18f6dee6220a3372f28b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-05-29 16:56:25 -0300

    Created __author__.py @2021-05-29 19:56

---
+++ __author__.py
@@ -0,0 +1,2 @@
+{
+"megan": {"author_nick": "katia", "author_name": "Katia GuimarÃ£es SOusa Palomo", "author_email": "profa.katia.palomo@gmail.com", "author_org": "Instituto Federal de BrasÃ\xadlia", "author_site": "www.ifb.edu.br", "author_public": "on"},
