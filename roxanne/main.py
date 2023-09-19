598e82f952fc1e4bc0570fdf6f37a2ea53a60683
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:27:24 -0300

    Saved roxanne/main.py @2023-09-19 15:27

--- roxanne/main.py
+++ roxanne/main.py
@@ -2,16 +2,17 @@
 
 from _spy.vitaline.main import Cena, Elemento, STYLE
 STYLE["width"] = 800
+STYLE["height"] = "600px"
 IMAGEM = "https://img.freepik.com/free-vector/retro-styled-pattern-background_1048-6593.jpg"
 PORTAO_BRONZE = "https://precoimbativel.net/portao-de-correr-de-aluminio-a-medida/8742-portao-austin-de-correr-aluminio-a-medida-preparado-para-motorizacao.html"
 PALACIO_CORAL = "https://www.parquesdesintra.pt/media/trwbbkth/parques_de_sintra_palacio_de_queluz_hero_01.jpg?rxy=0.49833333333333335,0.405&width=750&height=750&quality=90&format=webp&rnd=132442512038670000"
 PEAO = "https://lojagrow.vteximg.com.br/arquivos/ids/168597-1000-1000/Peao-Grow-Vermelho.png?v=637487474175630000"
 
 class IlhaProibida:
-	def __init__(self):
-     	oceano = Cena(imagem).vai()
-     	portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, tit="Portao de Bronze", cena=oceano)
-     	palacio = Elemento(PALACIO_CORAL, x=120, y=30, w=100, h=100, tit="Palacio de Coral", cena=oceano)
-     	peao = Elemento(PEAO, x=20, y=20, w=30, h=30, cena=oceano)
+    def __init__(self):
+         oceano = Cena(imagem).vai()
+         portao = Elemento(PORTAO_BRONZE, x=10, y=10, w=100, h=100, tit="Portao de Bronze", cena=oceano)
+         palacio = Elemento(PALACIO_CORAL, x=120, y=30, w=100, h=100, tit="Palacio de Coral", cena=oceano)
+         peao = Elemento(PEAO, x=20, y=20, w=30, h=30, cena=oceano)
         
 IlhaProibida()
