53cfdc872556a714ba13733eca14929bc5000ce3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-19 12:25:22 -0300

    Created roxanne/__log__.py @2023-09-19 15:25

---
+++ roxanne/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Sep 19 2023 12:25:19.833 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  oceano = Cena(imagem).vai()
+  ^
+IndentationError: expected an indented block
+'''},
