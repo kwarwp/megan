8a13198dfaa78f118690fbb504768c4f3d1036bc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-26 11:45:47 -0300

    Saved roxanne/main.py @2023-09-26 14:45

--- roxanne/main.py
+++ roxanne/main.py
@@ -2,13 +2,12 @@
 
 from _spy.vitollino.main import Cena, Elemento, STYLE
 
-STYLE["width"] = 800
+SSTYLE["width"] = 800
 STYLE["height"] = "600px"
-
-IMAGEM = "https://img.freepik.com/free-vector/retro-styled-pattern-background_1048-6593.jpg"
-PORTAO_BRONZE = "https://precoimbativel.net/494016-large_default/portao-pedonal-philadelphia-em-aluminio-a-medida.jpg"
-PALACIO_CORAL = "https://www.parquesdesintra.pt/media/trwbbkth/parques_de_sintra_palacio_de_queluz_hero_01.jpg?rxy=0.49833333333333335,0.405&width=750&height=750&quality=90&format=webp&rnd=132442512038670000"
-PEAO = "https://lojagrow.vteximg.com.br/arquivos/ids/168597-1000-1000/Peao-Grow-Vermelho.png?v=637487474175630000"
+IMAGEM = "https://imgur.com/gVHmY2v.jpg"
+PORTAO_BRONZE = "https://imgur.com/BL6lB7H.jpg"
+PALACIO_CORAL = "https://imgur.com/tLDbzd2.jpg"
+PAWN = "https://imgur.com/zO3kiRp.png"
 
 class IlhaProibida:
     def __init__(self):
