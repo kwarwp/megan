54ad1c9905b50e5e7cb98581b9550d29229d02a6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-26 11:45:52 -0300

    Saved roxanne/__log__.py @2023-09-26 14:45

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -204,3 +204,19 @@ NameError: name 'PAWN' is not defined
     self.peao = Elemento(PAWN, x=20, y=70, w=80, h= 80,
 NameError: name 'PAWN' is not defined
 '''},
+{'date': 'Tue Sep 26 2023 11:45:52.16 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    SSTYLE["width"] = 800
+NameError: name 'SSTYLE' is not defined
+'''},
